739aa09d92d6fcc939a5b585625913101a2ef1be
minSW <minsw08@gmail.com>
2021-07-02 19:51:06 +0900

    [#43] Add try except and Upload 210702 0AM output

--- project/dc_scraper.py
+++ project/dc_scraper.py
@@ -6,10 +6,12 @@ import requests
 from bs4 import BeautifulSoup
 from pytz import timezone
 
+from time import sleep
+
 START_DT = datetime.combine(datetime.now(timezone('Asia/Seoul')).date() - timedelta(days=1), time(6))
 
 URL = 'https://gall.dcinside.com/board/lists/'
-HEADERS = {'User-Agent': 'test'}
+HEADERS = {'User-Agent': 'testt'}
 MAX_INT = sys.maxsize
 OUTPUT_DIR = "output"
 INPUT_DIR = "output/board_ids.csv"
@@ -41,13 +43,7 @@ def get_contents_from_html(response):
 
 
 # Get post
-def get_post(content, num):
-    date_tag = content.find('td', class_='gall_date')
-    date = datetime.strptime(date_tag.attrs['title'], '%Y-%m-%d %H:%M:%S')
-    # not today
-    if date < START_DT:
-        return None
-
+def get_post(content, num, date):
     title = content.find('a').text.replace(",", " ") # replace ',' to blank
     view = int(content.find('td', class_='gall_count').text)
     recommend = int(content.find('td', class_='gall_recommend').text)
@@ -65,8 +61,9 @@ def get_post(content, num):
 def get_pages(board_id, output):
     is_today, prev_id = True, MAX_INT
     params = {'id': board_id, 'page': 0}
+    retry = 3
 
-    while is_today:
+    while is_today and retry > 0 :
         params['page'] += 1
         response = requests.get(URL, params=params, headers=HEADERS)
         print("scraping... page - {}".format(params['page']))
@@ -74,27 +71,65 @@ def get_pages(board_id, output):
         if response.status_code != 200:
             print("[ERROR] RESPONSE CODE : {}".format(response.status_code))
             return
+        try:
+            contents = get_contents_from_html(response)
 
-        contents = get_contents_from_html(response)
-        for content in contents:
-            num = content.find('td', class_='gall_num').text
-            if not num.isdigit() or int(num) >= prev_id:
-                continue
+            for content in contents:
+                num = content.find('td', class_='gall_num').text
+                date_tag = content.find('td', class_='gall_date')
 
-            c_id = int(num)
-            post = get_post(content, c_id)
+                if "운영자" in content.find('td', class_='gall_writer').text :
+                    continue
+                elif not num.isdigit() or int(num) >= prev_id:
+                    continue
+                elif not date_tag.has_attr('title') :
+                    continue
 
-            if not post:
-                is_today = False
-                break
+                c_id = int(num)
+                date = datetime.strptime(date_tag.attrs['title'], '%Y-%m-%d %H:%M:%S')
 
-            # File Write
-            output.write(post.to_csv_row() + "\n")
-            prev_id = c_id
+                if date < START_DT:
+                    is_today = False
+                    break
+
+                try:
+                    post = get_post(content, c_id, date)
+                    # File Write
+                    output.write(post.to_csv_row() + "\n")
+                    prev_id = c_id
+
+                except Exception as e:
+                    print("skip exception : ", e)
+                    print(content)
+        except Exception as e:
+            print("--------------------------")
+            if retry :
+                print("[ERROR] SKIP '{}' GALLARY".format(board_id))
+                return
+            else :
+                print("[WARNING] FAILED '{}' GALLARY ({})".format(board_id, params['page']))
+                print("[WARNING] SLEEP 10S AND RETRY (REMAIN RETRY COUNT : {}".foramt(retry))
+                sleep(10)
+                params['page'] -= 1
+                retry -= 1
 
     print("--------------------------")
-    print("SCRAPED PAGES : 1 ~ {}".format(params['page']))
+    print("SCRAPED PAGES : 1 ~ {}\n".format(params['page']))
 
+
+def single(gall_name):
+    start_time = datetime.now()
+    today = start_time.strftime("%Y%m%d")
+
+    output_path = f"{OUTPUT_DIR}/{today}/{gall_name}.csv"  # csv
+    o = open(output_path, "w")
+    print(f"Save to '{output_path}'")
+
+    o.write(Post().get_header()+"\n")
+    # get Today's Contents
+    get_pages(gall_name, o)
+    o.close()
+    print("[END] TOTAL TIME : ", datetime.now() - start_time)
 
 def main(gall_names):
     start_time = datetime.now()
@@ -120,7 +155,9 @@ def print_warning_empty_borad_ids_file():
 
 
 if __name__ == "__main__":
-    if not os.path.exists(f"{INPUT_DIR}"):
+    if len(sys.argv) >= 2 :
+        single(sys.argv[1])
+    elif not os.path.exists(f"{INPUT_DIR}"):
         print_warning_empty_borad_ids_file()
     else:
         inputList = []
