afbea2ef50e8f54de1639b3d2fbe1681536e8072
ChoMk <gmk5432@naver.com>
2021-10-07 20:41:23 +0900

    test

--- project/mk/front/src/Test/index.js
+++ project/mk/front/src/Test/index.js
@@ -20,14 +20,61 @@ const options = {
     fontWeight: "bold",
     fontSizes: [1, 50]
 };
+const scoreMap = new Map()
+let dates = []
+let wordsAndScore = []
 
-const getData = () => d3.csv(worddata, function (row) {
-    return {DATE: row.DATE, SCORE_WORDS: row.SCORE_WORDS.split(",")}
+const calculateScore = () => d3.csv(worddata, function (data) {
+    let conceptWeight = data.length + 1
+    console.log(1)
+    for (var i = 0; i < data.length; i++) {
+        data[i].BEST_WORDS.split(",").forEach(function (word) {
+            const wordAndScore = word.split("^")
+            data[i].GALL_ID.split(",").forEach(function (id) {
+                    const idAndScore = id.split("^")
+                    let wordScore = wordAndScore[1];
+                    let idScore = idAndScore[1]
+                    if (wordScore < 0) {
+                        wordScore = 0
+                    } else {
+                        wordScore = wordScore * 100
+                    }
+                    if (idScore < 0) {
+                        idScore = 0
+                    } else {
+                        idScore = idScore * 100
+                    }
+                    if (!scoreMap.has(idAndScore[0])) {
+                        const wordMap = new Map()
+                        wordMap.set(wordAndScore[0], 0)
+                        scoreMap.set(idAndScore[0], wordMap)
+                    }
+                    let wordMap = scoreMap.get(idAndScore[0])
+                    wordMap.set(wordAndScore[0], wordMap.get(wordAndScore[0]) + (Math.pow(wordScore * idScore, 2) * conceptWeight))
+                    scoreMap.set(idAndScore[0], wordMap)
+                }
+            )
+            conceptWeight -= 1
+        })
+
+    }
+    dates = Array.from(scoreMap.keys());
+    let values = Array.from(scoreMap.values());
+    values.forEach(function (scoreMap) {
+        let scoreMapKeys = Array.from(scoreMap.keys());
+        let scoreMapValues = Array.from(scoreMap.values());
+        let tempResult = []
+        for (let i = 0; i < scoreMapKeys.length; i++) {
+            tempResult.push(`${scoreMapKeys[i]} -> ${scoreMapValues[i]}`)
+        }
+        wordsAndScore.push(tempResult)
+    })
 })
+
 
 const getWords = (inputData) => {
     return inputData.map((data) => {
-        const splitData = data.split(" -> ")
+        const splitData = data.split(' -> ')
         return {text: splitData[0], value: (splitData[1])}
     })
 }
@@ -35,7 +82,6 @@ const getWords = (inputData) => {
 const getTitle = (data) => {
     return <li> {data} </li>
 }
-
 
 // 3. main component & state
 const initState = {
@@ -47,18 +93,18 @@ const initState = {
 const TestResult = React.memo(() => {
     // set inital state
     const [state, setState] = useState(initState)
-
+    calculateScore()
     // initial loading (when rendering)
-    useEffect(() => getData().then((d) => {
-        if (state.words.length === 0) {
+    useEffect(() => {
+        for (var i = 0; i < dates.length; i++) {
             setState({
                 ...state,
-                dates: (d.map((a) => getTitle(a.DATE))),
-                words: (d.map((a) => getWords(a.SCORE_WORDS)))
+                dates: (dates[i].map((a) => getTitle(a))),
+                words: (wordsAndScore[i].map((a) => getWords(a)))
             })
         }
-    }))
 
+    })
     const increaseIndex = () => {
         const nextIndex = (state.index < state.words.length - 1) ? state.index + 1 : 0
         setState({...state, index: nextIndex})
