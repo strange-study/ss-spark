ceb7742fb50058d4403b584867b5e2cf9b4344f1
myeongki.cho <myeongki.cho@navercorp.com>
2021-10-27 19:19:52 +0900

    N/A 코드리뷰 반영

--- project/copy_result_file_executor.py
+++ project/copy_result_file_executor.py
@@ -4,15 +4,15 @@ import os
 
 FILE_TYPE = "csv"
 CURRENT_DATE = datetime.today().strftime("%Y%m%d")
-USER_DIR_NAME = ["jj", "mk", "sw"]
+USER_DIR_NAME = ["jj", "mk", "minsw"]
 
 
 def copy_file(user_name):
     scala_output_path = f"./front/output/{user_name}/{CURRENT_DATE}"
     csv_file_list = [file for file in os.listdir(scala_output_path) if file.endswith(FILE_TYPE)]
-    for(csv_file) in csv_file_list:
-        result_file_name = f"{user_name}_{csv_file}"
-        shutil.copyfile(f"./front/output/{user_name}/{CURRENT_DATE}/{csv_file}", f"./front/src/resources/{result_file_name}")
+    for(idx) in range(len(csv_file_list)):
+        result_file_name = f"{user_name}_{idx+1}.{FILE_TYPE}"
+        shutil.copyfile(f"./front/output/{user_name}/{CURRENT_DATE}/{csv_file_list[idx]}", f"./front/src/resources/{result_file_name}")
 
 
 
