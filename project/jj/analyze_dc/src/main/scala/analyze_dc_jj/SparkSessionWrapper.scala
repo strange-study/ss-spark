97d69356fea79e638200b97486f09aface2aec54
jin5335 <jin5335@gmail.com>
2021-10-30 22:14:12 +0900

    change jj's spark config

--- project/jj/analyze_dc/src/main/scala/analyze_dc_jj/SparkSessionWrapper.scala
+++ project/jj/analyze_dc/src/main/scala/analyze_dc_jj/SparkSessionWrapper.scala
@@ -7,8 +7,6 @@ trait SparkSessionWrapper {
   val conf = new SparkConf()
     .set("spark.driver.memory", "1g")
     .set("spark.excutor.memory", "2g")
-    .set("spark.eventLog.enabled", "true")
-    .set("spark.eventLog.dir", "/Users/user/Documents/study/ss-spark/project/jj/analyze_dc/log")
     .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
 
   lazy val spark: SparkSession = {
